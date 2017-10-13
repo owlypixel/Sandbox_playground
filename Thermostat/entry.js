3f7a8cd7b4989b7247f75b8151743dfad8ca3912
Owlypixel <owlypixel@gmail.com>
2017-10-14 02:23:42 +0300

    finished termostat

--- Thermostat/entry.js
+++ Thermostat/entry.js
@@ -1,1 +1,27 @@
 import "./style.scss"
+
+const _R = document.getElementById('temp'), 
+_O = document.querySelector('output'), 
+_F = _R.parentNode, 
+OPTS = ['min', 'max'];
+
+let val = +_R.value;
+
+function update(s) {
+if(val !== +_R[OPTS[.5*(1 + s)]]) {
+val += s;
+
+_F.style.setProperty('--val', val);
+_R.setAttribute('value', val);
+_O.textContent = val;
+}
+};
+
+addEventListener('click', e => {
+const _T = e.target;
+
+if(_T.tagName.toLowerCase() === 'button') {
+e.preventDefault();
+update(+_T.style.getPropertyValue('--s'))
+}	
+}, false);
